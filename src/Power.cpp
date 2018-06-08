096315e5ed7e3f46befe16bc30d48662cc61f634
Erik <evanreusel@gmail.com>
2018-06-08 20:11:44 +0200

    Structure make over

--- src/Power.cpp
+++ src/Power.cpp
@@ -1,44 +1,35 @@
 #include <Arduino.h>
+#include <Battery.cpp>
 //Class for reading and outputting a power value, set by a potmeter.
+
 class Power
 {
-    const int MAXANALOGVALUE = 4096;
-    int pin;
-    int minPower;
-    int maxPower;
-    int setPower;
-    int analogValue;
-    void setup(int pin)
-    {
-      this->pin = pin;
-      pinMode(this->pin, INPUT);
-      Serial.println("Setup Potentiometer complete.");
-    }
-    void read()
-    {
-      this->analogValue = analogRead(this->pin);
-    }
-    void calcSetPower()
-    {
-      //val = ((percent * (max - min) / 100) + min
-      this->setPower = ((this->analogValue/this->MAXANALOGVALUE)* (this->maxPower - this->minPower));
-    }
-    void setMinPower(int minPower)
-    {
-      this->minPower = minPower;
-    }
-    void setMaxPower(int maxPower)
-    {
-      this->maxPower = maxPower;
-    }
-    int getSetPower()
-    {
-      return this->setPower;
-    }
+public:
 
-    void update()
-    {
-
-    }
+  void init(Battery* batt)
+  {
+    this->pBattery = batt;
+  }
+  void update()
+  {
+    this->calcMaxPower();
+  }
+  unsigned int getMaxPower()
+  {
+    return this->maxPower;
+  }
+  unsigned int getSetPower()
+  {
+    return this->setPower;
+  }
+private:
+   float coilResistance;
+   unsigned int maxPower;
+   Battery* pBattery;
+   void calcMaxPower()
+   {
+     this->maxPower = sq(this->pBattery->getVoltage())/this->coilResistance;
+   }
+   unsigned int setPower;
 
 };
